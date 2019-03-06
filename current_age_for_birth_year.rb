3bb9a5d6dd936c59a28a2c51798e64592708268b
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-06 01:29:49 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,8 +1,13 @@
 def current_age_for_birth_year(birth_year)
 <<<<<<< HEAD
+<<<<<<< HEAD
   return 1984 + 19 - birth_year
 end
 =======
   return 2003 - birth_year
 end
 >>>>>>> eb235bd4ba26a299a6566d1b4eb3fc2617e64d12
+=======
+  return 2003 - birth_year
+end
+>>>>>>> 3d67716459052b8869f691adaa0391381a3398ee
